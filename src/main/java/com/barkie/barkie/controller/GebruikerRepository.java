dbad2d7cd5408053ef8976c8cea34150a86e8335
joeynap <joeynap@hotmail.com>
2019-12-16 15:01:15 +0100

    gebruiker endpoints

--- src/main/java/com/barkie/barkie/controller/GebruikerRepository.java
+++ src/main/java/com/barkie/barkie/controller/GebruikerRepository.java
@@ -1,9 +1,15 @@
 package com.barkie.barkie.controller;
 
 import com.barkie.barkie.domein.Gebruiker;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface GebruikerRepository extends CrudRepository<Gebruiker, Long> {
+	
+	List<Gebruiker> findByGebruikersNaamAndPassword(String gebruikersNaam, String password);
+	
 }
--- src/main/java/com/barkie/barkie/view/GebruikerEndpoint.java
+++ src/main/java/com/barkie/barkie/view/GebruikerEndpoint.java
@@ -2,18 +2,26 @@ package com.barkie.barkie.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barkie.barkie.controller.GebruikerService;
+import com.barkie.barkie.domein.Gebruiker;
 
 @RestController
 public class GebruikerEndpoint {
 	@Autowired
     GebruikerService gs;
 	
-	@GetMapping("gebruiker/{naam}")
-	public void getGebruiker(@PathVariable String naam) {
-		System.out.println("in getGebruiker met:" + naam);
+	@PostMapping("gebruiker")
+	public long setGebruiker(@RequestBody Gebruiker newGebruiker) {
+		return gs.addGebruiker(newGebruiker);
+	}
+	
+	@GetMapping("gebruiker")
+	public long getGebruiker(@RequestBody Gebruiker queryGebruiker) {
+		long id = gs.getGebruiker(queryGebruiker);
+		return id;
 	}
 }
